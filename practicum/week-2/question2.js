e268ba70ee5e8dcd73bd7b65e98015185297f07d
EujinKo <42593556+EujinKo@users.noreply.github.com>
2020-09-08 07:38:55 -0700

    Update question2.js

--- practicum/week-2/question2.js
+++ practicum/week-2/question2.js
@@ -10,6 +10,27 @@ Example: input: [-12,3,-1,5,-2,1,-7]
           */
 
  var largestSubarray = function(input) {
-     
+     let i,j;
+     let max = 0;
+     for(i=0;i<input.length;i++){
+             for(j=0;j<input.length;j++){
+                     let tmp = input.slice(i,j);
+                     let sum = rtSum(tmp);
+                     if(sum>max){
+                             max = sum;
+                     }
+             }
+     }
+     return max;
     
  };
+
+ //Returns sum of the array
+ function rtSum(input){
+        let i;
+        var sum = 0;
+        for(i=0;i<input.length;i++){
+                sum += input[i];
+        }
+        return sum;
+ }
