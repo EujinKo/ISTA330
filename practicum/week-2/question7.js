5ee2395ade8cf1cffa761f631358afe205edb2d4
EujinKo <42593556+EujinKo@users.noreply.github.com>
2020-09-08 04:59:10 -0700

    7,8

--- practicum/week-2/question7.js
+++ practicum/week-2/question7.js
@@ -12,5 +12,19 @@ output: [[1,4], [2,5], [3,6]]
 */
 
 var transpose = function(M) {
+    let len_col = M[0].length;
+    let len_row = M.length;
+
+    let i,j;
+    var matrix = [];
+    var tmp;
+    for(i=0;i<len_col;i++){
+        tmp = [];
+        for(j=0;j<len_row;j++){
+            tmp.push(M[j][i]);
+        }
+        matrix.push(tmp);
+    }
+    return matrix;
 
 };
--- practicum/week-2/question8.js
+++ practicum/week-2/question8.js
@@ -13,5 +13,53 @@ output: 3
 */
 
 var d_integer = function(input) {
+    let list = [];
+    
+    let i;
+    for(i=0;i<input.length;i++){
+        if(findNinArray(list,input[i])==false){
+            list.push(input[i]);
+        }
+    }
+
+    let num_list = [];
+    for(i=0;i<list.length;i++){
+        num_list.push(findHowMany(input,list[i]));
+    }
+
+    let max = -1;
+    for(i=0;i<list.length;i++){
+        let val = list[i];
+        let num_val = num_list[i];
+        if(val===num_val && val>max){
+            max = val;
+        }
+    }
+    return max;
 
 };
+
+
+//Checks whether target value exists in input array
+function findNinArray(arr,target){
+    let i;
+    for(i=0;i<arr.length;i++){
+        if(arr[i]===target){
+            return true;
+        }
+    }
+    return false;
+}
+
+//Checks how many target value exists in input array
+//  and return it's number
+function findHowMany(arr,target){
+    let i;
+    let count = 0;
+    for(i=0;i<arr.length;i++){
+        if(arr[i]===target){
+            count += 1;
+        }
+    }
+    return count;
+}
