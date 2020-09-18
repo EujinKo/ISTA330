dda4caacfb2e2f6f7aaf75e2104b6aa212cad44f
EujinKo <42593556+EujinKo@users.noreply.github.com>
2020-09-18 07:32:08 -0700

    Update question6.js

--- practicum/week-3/question6.js
+++ practicum/week-3/question6.js
@@ -25,5 +25,45 @@ output:  4
 */
 
 var longestNested = function(A) {
+     var nested = [];
+     
+     let i;
+     for(i=0;i<A.length;i++){
+          let tmp_arr = [];
+          
+          let value = A[i];
+          while(existsDuplicate(tmp_arr,value) == false){
+               tmp_arr.push(value);
+               value = A[value];
 
+          }
+          alert(tmp_arr);
+          nested.push(tmp_arr);
+     }
+
+     let count = 0;
+     for(i=0;i<nested.length;i++){
+          let tmp = nested[i];
+          if(tmp.length>count){
+               count = tmp.length;
+          }
+     }
+     return count;
+
+};
+
+
+//Checks whether value target exists in the array input
+function existsDuplicate(input,target) {
+     let i;
+     for(i=0;i<input.length;i++){
+         tmp = input[i];
+     //     alert(tmp+","+target);
+         if(tmp == target){
+              return true;
+         }
+ 
+     }
+      return false;
+ 
 };
